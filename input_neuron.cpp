2b99f69308cc0db0f980319a3559a66407633be3
Oleg Silkin <o.silkin98@gmail.com>
2017-12-06 06:15:59 -0600

    Delete input_neuron.cpp

--- input_neuron.cpp
+++
@@ -1,9 +0,0 @@
-#include "input_neuron.h"
-
-// input function reads a value from the tile and
-// stores that value in the object
-void InputNeuron::input(const unsigned long val) {
-  value = static_cast<long double>(val);
-}
-
-
